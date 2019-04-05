2fcba3cddbaf02231350d50bfc6cfe790a5c6c07
Anatolii Iatsuk <35109939+tolik0@users.noreply.github.com>
2019-04-05 16:35:51 +0300

    Add class trainCache

---
+++ inc/trainCache.h
@@ -0,0 +1,21 @@
+//
+// Created by iatsuk on 05.04.2019.
+//
+
+#ifndef PLANN_TRAINCASHE_H
+#define PLANN_TRAINCASHE_H
+
+#include "matrix.h"
+
+class TrainCache {
+    Matrix w;
+    Matrix dw;
+    Matrix z;
+    Matrix dz;
+    Matrix at;
+    Matrix wt;
+
+    void TrainCache(int previous_units_number, int current_units_number, int batch_size);
+};
+
+#endif //PLANN_TRAINCASHE_H
---
+++ src/trainCache.cpp
@@ -0,0 +1,23 @@
+//
+// Created by iatsuk on 05.04.2019.
+//
+
+#include "trainCache.h"
+
+class trainCache{
+    Matrix w;
+    Matrix dw;
+    Matrix z;
+    Matrix dz;
+    Matrix at;
+    Matrix wt;
+
+    void TrainCache(int previous_units_number, int current_units_number, int batch_size){
+        this->w = new Matrix(previous_units_number, current_units_number);
+        this->dw = new Matrix(previous_units_number, current_units_number);
+        this->wt = new Matrix(current_units_number, previous_units_number);
+        this->z = new Matrix(current_units_number, batch_size);
+        this->dz = new Matrix(current_units_number, batch_size);
+        this->at = new Matrix(batch_size, current_units_number);
+    }
+};
