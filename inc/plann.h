75730c949f86dc35aa16baf6492e6757eb8362fe
ostapViniavskyi <viniavskyi@ucu.edu.ua>
2019-04-08 17:06:08 +0300

    Add single header file

---
+++ inc/plann.h
@@ -0,0 +1,14 @@
+//
+// Created by ostap on 08.04.19.
+//
+
+#ifndef PLANN_PLANN_H
+#define PLANN_PLANN_H
+
+#include "activation.h"
+#include "model.h"
+#include "layer.h"
+#include "loss.h"
+#include "matrix.h"
+
+#endif //PLANN_PLANN_H
