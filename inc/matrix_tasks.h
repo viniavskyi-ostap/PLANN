70cc7ba78f4d1540d0da47b4704399854cbf63bd
ostapViniavskyi <viniavskyi@ucu.edu.ua>
2019-05-17 16:21:32 +0300

    Add const references for source matrices

--- inc/matrix_tasks.h
+++ inc/matrix_tasks.h
@@ -7,9 +7,9 @@
 
 #include "parallel_matrix.h"
 
-void map_task(const std::function<float(float)> &f, Matrix *source,  Matrix *result, int start, int end);
+void map_task(const std::function<float(float)> &f, const Matrix *source,  Matrix *result, int start, int end);
 
-void combine_task(const std::function<float(float, float)> &f, Matrix *source1, Matrix *source2,  Matrix *result, int start, int end);
+void combine_task(const std::function<float(float, float)> &f, const Matrix *source1,const Matrix *source2,  Matrix *result, int start, int end);
 
 void map_indexed_task(const std::function<float(float, int, int)> &f, Matrix *source,  Matrix *result, int start, int end);
 
@@ -18,4 +18,8 @@ void transpose_task(Matrix *source, Matrix *result, int start, int end);
 void reduce_row_task(std::function<float(float, float)> f, Matrix *source, Matrix *result, float initial_value, int start, int end);
 
 void add_column(Matrix *source, Matrix *rhs, int start, int end);
+
+float sum_task(Matrix *source, int start, int end);
+
+void multiply_task(Matrix *source1, Matrix *source2, Matrix *result, int start, int end);
 #endif //PLANN_MATRIX_TASKS_H
--- src/matrix_tasks.cpp
+++ src/matrix_tasks.cpp
@@ -4,7 +4,7 @@
 void map_task(const std::function<float(float)> &f, Matrix *source, Matrix *result, int start, int end) {
     int row_size = source->get_columns_number();
 
-    for (int i = 0; i < end - start; ++i) {
+    for (int i = start; i < end; ++i) {
         for (int j = 0; j < row_size; ++j) {
             result->set(i, j, f(source->get(i, j)));
         }
@@ -15,7 +15,7 @@ void combine_task(const std::function<float(float, float)> &f, Matrix *source1, 
                   int start, int end) {
     int row_size = source1->get_columns_number();
 
-    for (int i = 0; i < end - start; ++i) {
+    for (int i = start; i < end; ++i) {
         for (int j = 0; j < row_size; ++j) {
             result->set(i, j, f(source1->get(i, j), source2->get(i, j)));
         }
@@ -26,29 +26,29 @@ void map_indexed_task(const std::function<float(float, int, int)> &f, Matrix *so
                       int start, int end) {
     int row_size = source->get_columns_number();
 
-    for (int i = 0; i < end - start; ++i) {
+    for (int i = start; i < end; ++i) {
         for (int j = 0; j < row_size; ++j) {
             result->set(i, j, f(source->get(i, j), i, j));
         }
     }
 }
 
-void transpose_task(Matrix *source, Matrix *result, int start, int end){
+void transpose_task(Matrix *source, Matrix *result, int start, int end) {
     int row_size = source->get_columns_number();
 
-    for (int i = 0; i < end - start; ++i) {
+    for (int i = start; i < end; ++i) {
         for (int j = 0; j < row_size; ++j) {
             result->set(j, i, source->get(i, j));
         }
     }
 }
 
-void reduce_row_task(std::function<float(float, float)> f, Matrix * source,  Matrix *result,
-        float initial_value, int start, int end){
+void reduce_row_task(std::function<float(float, float)> f, Matrix *source, Matrix *result,
+                     float initial_value, int start, int end) {
     int row_size = source->get_columns_number();
     float current_result;
 
-    for (int i = 0; i < end - start; ++i) {
+    for (int i = start; i < end; ++i) {
         current_result = initial_value;
         for (int j = 0; j < row_size; ++j) {
             current_result = f(current_result, source->get(i, j));
@@ -57,11 +57,11 @@ void reduce_row_task(std::function<float(float, float)> f, Matrix * source,  Mat
     }
 }
 
-void add_column(Matrix *source, Matrix *rhs, int start, int end){
+void add_column(Matrix *source, Matrix *rhs, int start, int end) {
     int row_size = source->get_columns_number();
     float current_summand;
 
-    for (int i = 0; i < end - start; ++i) {
+    for (int i = start; i < end; ++i) {
         current_summand = rhs->get(i, 0);
         for (int j = 0; j < row_size; ++j) {
             source->set(i, j, source->get(i, j) + current_summand);
@@ -69,3 +69,34 @@ void add_column(Matrix *source, Matrix *rhs, int start, int end){
     }
 }
 
+float sum_task(Matrix *source, int start, int end) {
+    int row_size = source->get_columns_number();
+    float result = 0;
+
+    for (int i = start; i < end; ++i) {
+        for (int j = 0; j < row_size; ++j) {
+           result += source->get(i, j);
+        }
+    }
+    return result;
+}
+
+void multiply_task(Matrix *source1, Matrix *source2, Matrix *result, int start, int end){
+    int source1_row_size = source1->get_columns_number();
+    int source2_row_size = source2->get_columns_number();
+
+    for (int i = start; i < end; ++i) {
+        for (int j = 0; j < source2_row_size; ++j) {
+            result->set(i, j, 0);
+        }
+    }
+
+    for (int i = start; i < end; ++i) {
+        for (int j = 0; j < source1_row_size; ++j) {
+            for (int k = 0; k < source2_row_size; ++k) {
+                result->set(i, k, result->get(i, k) + source1->get(i, j) * source2->get(j, k));
+            }
+        }
+    }
+
+}
